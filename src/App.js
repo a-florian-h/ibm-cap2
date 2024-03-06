5e8c8e6d468676c5b5d7aa52f40fa54cd79d3c11
Flo H <a.florian.harvey@gmail.com>
2024-03-06 17:02:30 +1100

    implementing routes and layout route

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from './Components/Navbar/Navbar';
+import Navbar from "./Components/Navbar/Navbar";
+import Layout from "./Components/Landing_Page/Landing_Page";
 
 function App() {
   return (
     <div className="App">
-    <BrowserRouter>
-      <Navbar/>
-          <Routes>
-          </Routes>
-        
-    </BrowserRouter>
-   
-</div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Layout />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
