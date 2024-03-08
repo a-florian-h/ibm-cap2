a84547aa03a4035dd6413a75759e0c1fe93fd3c2
a-florian-h <a.florian.harvey@gmail.com>
2024-03-08 01:18:24 -0500

    doctor search component

--- src/App.js
+++ src/App.js
@@ -6,8 +6,14 @@ import Layout from "./Components/Landing_Page/Landing_Page";
 import Sign_Up from "./Components/Sign_Up/Sign_Up";
 import Login from "./Components/Login/Login";
 import InstantConsultation from './Components/InstantConsultationBooking/InstantConsultation'; 
+import FindDoctorSearch from "./Components/FindDoctorSearch/FindDoctorSearch";
+import DoctorCard from "./Components/DoctorCard/DoctorCard";
+import { doctors } from "./Components/DoctorCard/doctorsdata";
 
 function App() {
+
+
+
   return (
     <div className="App">
       <BrowserRouter>
@@ -17,6 +23,26 @@ function App() {
           <Route path="/sign_up" element={<Sign_Up />} />
           <Route path="/login" element={<Login />} />
           <Route path="/instant-consultation" element={<InstantConsultation />} />
+          <Route path="/finddoctorsearch" element={<FindDoctorSearch />} />
+          {/* <Route path="/doctorcard" element={<DoctorCard />} /> */}
+            <Route
+                path="/doctorcard"
+                element={
+                <>
+                {doctors.map((doctor, index) => (
+                <DoctorCard 
+                key={index}
+                name={doctor.name}
+                speciality={doctor.speciality}
+                experience={doctor.experience}
+                ratings={doctor.ratings}
+                // Add profilePic if available
+                />
+                ))}
+                </>
+                }
+            />
+          
         </Routes>
       </BrowserRouter>
     </div>
